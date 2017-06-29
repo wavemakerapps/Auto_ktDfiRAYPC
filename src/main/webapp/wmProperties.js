751d2d3da825ebe20bb51539de3c30104aea2346
root <root@40ee80ac6db9>
2017-06-29 09:22:30 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_ktDfiRAYPC",
+  "homePage" : "Main",
+  "name" : "Auto_ktDfiRAYPC",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
